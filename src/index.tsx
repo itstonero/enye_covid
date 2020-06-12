ab6bba8f167509e8b6866624aff05fc7494c1680
Asadu Anthony <asadu.anthony@outlook.com>
2020-06-12 14:00:16 +0100

    Initialize Application with Ant Design

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-const man = 9;
-const woman:number = 9;
 
 ReactDOM.render(
   <React.StrictMode>
