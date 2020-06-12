887ece2e0e1a4b6181c94049da8b5679d8507dd6
Asadu Anthony <asadu.anthony@outlook.com>
2020-06-12 19:42:43 +0100

    Added Hospital and Locator Models

---
+++ src/models/hospital.ts
@@ -0,0 +1,8 @@
+export interface Hospital
+{
+    name:string;
+    address:string;
+    email:string;
+    distanceFromLocation:number;
+    number:string;
+}
---
+++ src/models/locator.ts
@@ -0,0 +1,7 @@
+export interface Locator
+{
+    address:string;
+    latitude:number;
+    longitude:number;
+    radius:number;
+}
